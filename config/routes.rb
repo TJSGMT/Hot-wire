f2e1c4bc4c9dab4e7daea64c09307ea3ad7bc0d2
tjs <tejas.g@simformsolutions.com>
2022-08-24 19:36:56 +0530

    chat room using turbo stream

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'messages/new'
+  get 'messages/create'
+  resources :rooms do
+    resources :messages
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
